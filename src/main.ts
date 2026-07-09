60aa8508646ba7ecef95763cca4925a778721776
zerotwo02255 <ankuwizard5@gmail.com>
2026-07-09 21:11:44 +0545

    practice

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+console.log("🎓 Student Management System");
+
+
+/*const student = [{
+
+
+  name: "hari",
+  email: "hari@gmail.com",
+  IsEnrolled: true,
+  Grade: 9.5,
+  age: 22,
+  course: "BBA"
+
+},
+{
+  name: "ram",
+  email: "ram@gmail.com",
+  IsEnrolled: true,
+  Grade: 10.5,
+  age: 25,
+  course: "BBS"
+
+},
+
+{
+  name: "Sita",
+  email: "sita@gmail.com",
+  age: 23,
+  Grade: 85,
+  IsEnrolled: false,
+  course :"science"
+},
+
+
+
+
+]//
+console.log(student[0].name);
+console.log(student[0].email);
+console.log(student[0].age);
+console.log(student[0].IsEnrolled);
+console.log(student[0].Grade);
+console.log(student[0].course);
+
+console.log(student[1].name);
+console.log(student[1].email);
+console.log(student[1].age);
+console.log(student[1].IsEnrolled);
+console.log(student[1].Grade);
+console.log(student[1].course);
+
+console.log(student[2].name);
+console.log(student[2].email);
+console.log(student[2].age);
+console.log(student[2].IsEnrolled);
+console.log(student[2].Grade);
+console.log(student[2].course);
+
+*/
+
+
+console.log("🎓 Student Management System");
+
+function helllll( name :string ,grade :number){
+  console.log(" hello my name is "+ name + "and i have got" + grade);
+}
+
+helllll("ram " ,123);
+helllll("jeff " ,567)
+
